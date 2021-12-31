824c9d81ac9e3af98aef772b71034b0a3705c100
ThuLy1502 <chutaza7891@gmail.com>
2021-12-31 07:57:39 +0700

    Thêm menu Thêm strings.xml Thêm icon drawable Thêm code MainActivity.java

---
+++ app/src/main/java/vn/edu/stu/doanandroid/HomePageActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.stu.doanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
